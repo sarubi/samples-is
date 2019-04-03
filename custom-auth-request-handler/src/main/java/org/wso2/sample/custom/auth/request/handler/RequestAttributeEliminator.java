35269628365628e2c189b0233c4e8056d074ff16
sarubi <stsarut@gmail.com>
2019-04-03 13:53:43 +0530

    refactored

--- custom-auth-request-handler/src/main/java/org/wso2/sample/custom/auth/request/handler/RequestAttributeEliminator.java
+++ custom-auth-request-handler/src/main/java/org/wso2/sample/custom/auth/request/handler/RequestAttributeEliminator.java
@@ -1,6 +1,7 @@
 package org.wso2.sample.custom.auth.request.handler;
 
 import org.apache.commons.lang.StringUtils;
+import org.wso2.carbon.identity.application.authentication.framework.config.ConfigurationFacade;
 import org.wso2.carbon.identity.application.authentication.framework.context.AuthenticationContext;
 import org.wso2.carbon.identity.application.authentication.framework.exception.FrameworkException;
 import org.wso2.carbon.identity.application.authentication.framework.handler.request.impl.DefaultAuthenticationRequestHandler;
@@ -11,8 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * This custom class is to remove the "authenticators" query parameter if exits in the
- * /authenticationendpoint/login.do request. Then redirect to the login page. However need to customize the
+ * This custom class is to remove the "authenticators" query parameter if exits in the login request. The login page is
+ * defined in application-authentication.xml under AuthenticationEndpointURL.
+ * <AuthenticationEndpointURL>/authenticationendpoint/login.do</AuthenticationEndpointURL>
+ * After removed the authentication list, then redirect to the login page. However need to customize the
  * authentication web app handle this login request and implement a custom login to return the authenticators list.
  */
 public class RequestAttributeEliminator extends DefaultAuthenticationRequestHandler {
@@ -23,10 +26,12 @@ public class RequestAttributeEliminator extends DefaultAuthenticationRequestHand
 
         super.handle(request, response, context);
 
+        String loginPage = ConfigurationFacade.getInstance().getAuthenticationEndpointURL();
+
         if (response instanceof CommonAuthResponseWrapper) {
             if (((CommonAuthResponseWrapper) response).isRedirect()) {
                 String redirectUrl = ((CommonAuthResponseWrapper) response).getRedirectURL();
-                if (StringUtils.isNotBlank(redirectUrl) && redirectUrl.contains("/authenticationendpoint/login.do")) {
+                if (StringUtils.isNotBlank(redirectUrl) && redirectUrl.contains(loginPage)) {
                     redirectUrl = removeAuthenticatorsQueryParam(redirectUrl);
                 }
                 // Set the modified redirect URL
